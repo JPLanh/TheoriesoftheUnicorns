6604e2a1344cd5dbf02c6f2cd6f811f427785ba0
JPLanh <JPLanh@yahoo.com>
2018-03-04 20:47:24 -0800

    Add files via upload
    Try to follow the assignment as much as possible, created a Mydecrypt module, and move the decrypting processes in the module.

--- SingleEncryption/decrypt.py
+++ SingleEncryption/decrypt.py
@@ -6,23 +6,9 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
 
-def MyfileDecrypt(filepath):
-  #break the file name to two parts, the name and extension
-  fName, fExt = os.path.splitext(os.path.basename(filepath))
-  jread = open(fName + ".json", 'r')
-  jsonStuff = json.load(jread)
-  jread.close()
- 
-  filename = jsonStuff['fileName'] + jsonStuff['ext']
-  if filename == fName + fExt:
-    f = open(filename, 'rb')
-    #store the read byte into data
-    data = f.read()
-    f.close()
-
+def Mydecrypt(cipherText, key, iv):
     backend = default_backend()
-    key = b64decode(jsonStuff["key"])
-    iv = b64decode(jsonStuff["iv"])
+
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
 
     
@@ -31,12 +17,43 @@ def MyfileDecrypt(filepath):
     #as we pad thing we have to unpad them
     unpadder = padding.PKCS7(128).unpadder()
 
-    pt = decryptor.update(data)  + decryptor.finalize()
+    pt = decryptor.update(cipherText)  + decryptor.finalize()
     pt = unpadder.update(pt) + unpadder.finalize()
 
+    print(" > Decrypting complete")
+    return pt
+  
+def MyfileDecrypt(filepath):
+  print(" > Locating top secret json information")
+  #break the file name to two parts, the name and extension
+  fName, fExt = os.path.splitext(os.path.basename(filepath))
+  jread = open(fName + ".json", 'r')
+  jsonStuff = json.load(jread)
+  jread.close()
+
+  filename = jsonStuff['fileName'] + jsonStuff['ext']
+  if filename == fName + fExt:
+    print(" > top secret json information location")
+    print(" > opening " + filepath)
+    f = open(filename, 'rb')
+    #store the read byte into data
+    data = f.read()
+    f.close()
+
+    key = b64decode(jsonStuff["key"])
+    iv = b64decode(jsonStuff["iv"])
+
+    print(" > Decrypting " + filepath)
+    pt = Mydecrypt(data, key, iv)
+
+    print(" > Writing plaintext to file")    
     #convert it back from a byte and make it into a image
     f = open(filename, 'wb')
     f.write(pt)
     f.close()
+
+    print(" > Encryption process complete")
+
+    return pt, fExt
   else:
     print("File does not exist, or it wasn't our fault that this file was corrupted because we did not touch it")
